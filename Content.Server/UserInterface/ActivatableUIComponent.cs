98a8489ca651066099b5c572a921aaa6add9a82a
Silver <silvertorch5@gmail.com>
2022-03-03 12:31:34 -0700

    Purge ISerializationHooks from ActivatableUI

--- Content.Server/UserInterface/ActivatableUIComponent.cs
+++ Content.Server/UserInterface/ActivatableUIComponent.cs
@@ -7,10 +7,10 @@ using Robust.Shared.Serialization;
 namespace Content.Server.UserInterface
 {
     [RegisterComponent]
-    public sealed class ActivatableUIComponent : Component,
-            ISerializationHooks
+    public sealed class ActivatableUIComponent : Component
     {
         [ViewVariables]
+        [DataField("key", readOnly: true, required: true, customTypeSerializer: typeof(EnumReferenceSerializer))]
         public Enum? Key { get; set; }
 
         [ViewVariables] public BoundUserInterface? UserInterface => (Key != null) ? Owner.GetUIOrNull(Key) : null;
@@ -26,9 +26,6 @@ namespace Content.Server.UserInterface
         [ViewVariables(VVAccess.ReadWrite)]
         [DataField("adminOnly")]
         public bool AdminOnly { get; set; } = false;
-
-        [DataField("key", readOnly: true, required: true)]
-        private string _keyRaw = default!;
 
         [DataField("verbText")]
         public string VerbText = "ui-verb-toggle-open";
@@ -56,12 +53,6 @@ namespace Content.Server.UserInterface
         [ViewVariables]
         public IPlayerSession? CurrentSingleUser;
 
-        void ISerializationHooks.AfterDeserialization()
-        {
-            var reflectionManager = IoCManager.Resolve<IReflectionManager>();
-            if (reflectionManager.TryParseEnumReference(_keyRaw, out var key))
-                Key = key;
-        }
     }
 }
 
